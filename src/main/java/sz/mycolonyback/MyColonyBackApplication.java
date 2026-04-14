3573de3cc40f30862eb0afc937a1f33af56c82ac
stanislau.zhvanski <stanislau.zhvanski@idfeurasia.pro>
2026-04-14 15:35:19 +0300

    Initial commit

---
+++ src/main/java/sz/mycolonyback/MyColonyBackApplication.java
@@ -0,0 +1,13 @@
+package sz.mycolonyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyColonyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyColonyBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sz/mycolonyback/MyColonyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package sz.mycolonyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyColonyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
