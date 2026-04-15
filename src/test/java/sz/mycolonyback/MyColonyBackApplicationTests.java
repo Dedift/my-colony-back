cd72fd24b1bb4a87543709e97646bb6d44f8dd81
stanislau.zhvanski <stanislau.zhvanski@idfeurasia.pro>
2026-04-15 08:21:32 +0300

    infra

--- src/test/java/sz/mycolonyback/MyColonyBackApplicationTests.java
+++ src/test/java/sz/mycolonyback/MyColonyBackApplicationTests.java
@@ -2,8 +2,10 @@ package sz.mycolonyback;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MyColonyBackApplicationTests {
 
 	@Test
